8168814b35801ee2c7ed0805ff33c4475813c35b
Kevin Paul <kpaul@ucar.edu>
2021-02-12 15:10:37 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+from sphinx_pythia_theme import __version__
+
+with open('README.md', 'rt') as fobj:
+    long_description = fobj.read()
+
+setup(
+    name="sphinx-pythia-theme",
+    version=__version__,
+    description="Sphinx Pythia Theme.",
+    long_description=long_description,
+    url="https://github.com/ProjectPythia/sphinx-pythia-theme",
+    author="Kevin Paul",
+    author_email="kpaul@ucar.edu",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Documentation",
+    ],
+
+    install_requires=[
+        "setuptools",
+    ],
+    entry_points = {
+        'sphinx.html_themes': [
+            'pythia = sphinx_pythia_theme',
+        ]
+    },
+    packages=["sphinx_pythia_theme"],
+    include_package_data=True,
+)
