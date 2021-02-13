ec0d1ed101bafeaebd1a6785219c92f64e592293
Kevin Paul <kpaul@ucar.edu>
2021-02-13 15:16:58 -0700

    Sphinx installation

--- sphinx_pythia_theme/__init__.py
+++ sphinx_pythia_theme/__init__.py
@@ -1,8 +1,16 @@
 import os
+from pathlib import Path
+
+from sphinx.application import Sphinx
 
 __version__ = "0.0.1"
 
-def setup(app):
-    if hasattr(app, 'add_html_theme'):
-        theme_path = os.path.abspath(os.path.dirname(__file__))
-        app.add_html_theme('pythia', os.path.join(theme_path, 'pythia'))
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    theme_path = os.path.abspath(Path(__file__).parent)
+    return theme_path
+
+
+def setup(app: Sphinx):
+    app.add_html_theme('sphinx_pythia_theme', get_html_theme_path())
