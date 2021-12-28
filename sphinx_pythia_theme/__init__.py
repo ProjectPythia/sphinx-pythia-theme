3f0f93f7ba9da96a6f412736a771ceee98f00d41
Kevin Paul <kpaul@ucar.edu>
2021-12-28 12:57:32 -0700

    Remove special formatting function

--- sphinx_pythia_theme/__init__.py
+++ sphinx_pythia_theme/__init__.py
@@ -1,10 +1,8 @@
-import copy
 import os
 import shutil
 from pathlib import Path
 from pkg_resources import get_distribution, DistributionNotFound
 
-from bs4 import BeautifulSoup
 from sphinx.application import Sphinx
 
 from .banner import Banner
@@ -35,51 +33,6 @@ def copy_config_images(app):
             logos_config[key] = copy_image(app, image)
 
 
-def fix_theme_options(app, pagename, templatename, context, doctree):
-    context["theme_denest_depth"] = int(context.get("theme_denest_depth", 2))
-
-    order = context.get("theme_denest_split", "True").lower()
-    context["theme_denest_split"] = False if order == "false" else True
-
-
-def add_functions_to_context(app, pagename, templatename, context, doctree):
-    def denest_sections(html, maxdepth=2, split=True):
-        if maxdepth < 2:
-            return html
-
-        soup = BeautifulSoup(html, "html.parser")
-
-        hnames = [f"h{i}" for i in range(2, maxdepth + 1)]
-        htags = soup.find_all(hnames)
-        htags.sort(key=lambda x: x.name)
-
-        for htag in htags:
-
-            div_section = htag.find_parent("div", {"class": "section"})
-            if div_section is None:
-                continue
-
-            div_parent = div_section.parent
-
-            if split:
-                div_remainder = soup.new_tag(div_parent.name)
-                div_remainder.attrs = copy.copy(div_parent.attrs)
-                del div_remainder["id"]
-                remainder = [sibling for sibling in div_section.next_siblings]
-                div_remainder.extend(remainder)
-
-            div_section = div_section.extract()
-
-            div_parent.insert_after(div_section)
-
-            if split:
-                div_section.insert_after(div_remainder)
-
-        return str(soup)
-
-    context["denest_sections"] = denest_sections
-
-
 def copy_image(app, image):
     conf_dir = Path(app.confdir)
     out_dir = Path(app.outdir)
@@ -99,8 +52,6 @@ def setup(app: Sphinx):
     app.add_html_theme("sphinx_pythia_theme", get_html_theme_path())
     app.add_directive("banner", Banner)
     app.connect("builder-inited", copy_config_images)
-    app.connect("html-page-context", fix_theme_options)
-    app.connect("html-page-context", add_functions_to_context)
 
     return {
         "version": "0.0",
