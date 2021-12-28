3a1e9ce37b4e179dc7574b4214e2d32ae163ba3e
Kevin Paul <kpaul@ucar.edu>
2021-12-28 10:26:47 -0700

    More straightforward denesting

--- sphinx_pythia_theme/__init__.py
+++ sphinx_pythia_theme/__init__.py
@@ -22,62 +22,6 @@ def get_html_theme_path():
     return theme_path
 
 
-def add_functions_to_context(app, pagename, templatename, context, doctree):
-
-    def _is_div_section(tag):
-        return tag.name == 'div' and tag.has_attr('class') and 'section' in tag['class']
-
-    def _new_tag_like(tag):
-        if isinstance(tag, BeautifulSoup):
-            return BeautifulSoup()
-        elif isinstance(tag, NavigableString):
-            return copy.copy(tag)
-        else:
-            tag_copy = tag.find_parents()[-1].new_tag(tag.name)
-            tag_copy.attrs = copy.copy(tag.attrs)
-            return tag_copy
-
-    def _children_of(tag):
-        return [] if isinstance(tag, NavigableString) else tag.children
-
-    def _denest_sections_from(tag, maxdepth=2, _depth=1, order=True):
-        new_tag = _new_tag_like(tag)
-        new_tags = [new_tag]
-        for child in _children_of(tag):
-            if _is_div_section(child) and _is_div_section(tag) and _depth < maxdepth:
-                new_tags.extend(_denest_sections_from(child, maxdepth=maxdepth, _depth=_depth+1, order=order))
-                if order:
-                    new_tag = _new_tag_like(tag)
-                    del new_tag['id']
-                    new_tags.append(new_tag)
-            else:
-                new_tag.extend(_denest_sections_from(child, maxdepth=maxdepth, _depth=_depth, order=order))
-        return new_tags
-
-    def denest_sections(html, maxdepth=2, preserve_order=True):
-        soup = BeautifulSoup(html, 'html.parser')
-        new_soup = _new_tag_like(soup)
-        for child in soup.children:
-            new_soup.extend(_denest_sections_from(child, maxdepth=maxdepth, order=preserve_order))
-        return str(new_soup)
-
-    context["denest_sections"] = denest_sections
-
-
-def copy_image(app, image):
-    conf_dir = Path(app.confdir)
-    out_dir = Path(app.outdir)
-    old_img = conf_dir / image
-    if old_img.is_file():
-        new_dir = out_dir / "_images"
-        os.makedirs(new_dir, exist_ok=True)
-        new_img = Path(new_dir) / old_img.name
-        shutil.copy(old_img, new_img)
-        return str(Path("_images") / old_img.name)
-    else:
-        raise FileNotFoundError(f"Image file not found: {old_img}")
-
-
 def copy_config_images(app):
     if hasattr(app.config, "html_theme_options"):
         config = app.config.html_theme_options
@@ -95,8 +39,61 @@ def copy_config_images(app):
 def fix_theme_options(app, pagename, templatename, context, doctree):
     context["theme_denest_depth"] = int(context.get("theme_denest_depth", 2))
 
-    order = context.get("theme_denest_order", "True").lower()
-    context["theme_denest_order"] = False if order == "false" else True
+    order = context.get("theme_denest_split", "True").lower()
+    context["theme_denest_split"] = False if order == "false" else True
+
+
+def add_functions_to_context(app, pagename, templatename, context, doctree):
+
+    def denest_sections(html, maxdepth=2, split=True):
+        if maxdepth < 2:
+            return html
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        hnames = [f'h{i}' for i in range(2, maxdepth+1)]
+        htags = soup.find_all(hnames)
+        htags.sort(key=lambda x: x.name)
+
+        for htag in htags:
+
+            div_section = htag.find_parent('div', {'class': 'section'})
+            if div_section is None:
+                continue
+
+            div_parent = div_section.parent
+
+            if split:
+                div_remainder = soup.new_tag(div_parent.name)
+                div_remainder.attrs = copy.copy(div_parent.attrs)
+                del div_remainder['id']
+                remainder = [sibling for sibling in div_section.next_siblings]
+                div_remainder.extend(remainder)
+
+            div_section = div_section.extract()
+
+            div_parent.insert_after(div_section)
+
+            if split:
+                div_section.insert_after(div_remainder)
+
+        return str(soup)
+
+    context["denest_sections"] = denest_sections
+
+
+def copy_image(app, image):
+    conf_dir = Path(app.confdir)
+    out_dir = Path(app.outdir)
+    old_img = conf_dir / image
+    if old_img.is_file():
+        new_dir = out_dir / "_images"
+        os.makedirs(new_dir, exist_ok=True)
+        new_img = Path(new_dir) / old_img.name
+        shutil.copy(old_img, new_img)
+        return str(Path("_images") / old_img.name)
+    else:
+        raise FileNotFoundError(f"Image file not found: {old_img}")
 
 
 def setup(app: Sphinx):
