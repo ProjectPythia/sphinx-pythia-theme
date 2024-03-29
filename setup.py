28bfc7fa445b51b4b1e58e3ced676736666e46de
Orhan Eroglu <32553057+erogluorhan@users.noreply.github.com>
2024-03-28 22:26:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     install_requires=[
         "sphinx-book-theme>=1.0.0",
         "sphinx>=5.0",
+        "sphinx-design",
     ],
     packages=find_packages(),
     entry_points={"sphinx.html_themes": ["sphinx_pythia_theme = sphinx_pythia_theme"]},
